03a4adc4de342635fbe2033f64d5eeaba2384963
Arnovii <arnovi.jimenez@utp.edu.co>
2024-09-07 12:18:00 -0500

    Data folder created, basic structure for parametersInfo.py

---
+++ data/parametersInfo.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class ParametersInformation:
+    data = {
+        "1": {
+            "n": 400,
+            "w_range": np.arange(1,20) #en secondPoint le pasamos este rango, y ya en aux se encarga de recibir el rango y determinar el array concreto
+        }
+        
+        
+        
+    }
